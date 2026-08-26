1ad8a6b28d33be4de31689635e871ab1f1b1b5c1
sunny1901kumar-sys <sunny1901kumar@gmail.com>
2026-08-26 10:16:12 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,49 @@
+const user = {
+    username: "sunny",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage();
+
+// console.log(this);
+
+
+// function chai(){
+//     let username = "sunny"
+//     console.log(this.username);
+// }
+// chai()
+// const chai = function (){
+//     let username = "sunny"
+//     console.log(this.username);
+// }
+const chai = () => {
+    let username = "sunny"
+    console.log(this);
+}
+// chai()
+
+
+// const addTwo = (num1,num2) => {
+//     return num1 + num2
+
+// }
+// const addTwo = (num1,num2) =>  num1 + num2
+// const addTwo = (num1,num2) => ( num1 + num2)
+
+const addTwo = (num1,num2) =>({username: "sunny"})
+   
+
+
+console.log(addTwo(3,4))
+
+
+
+
