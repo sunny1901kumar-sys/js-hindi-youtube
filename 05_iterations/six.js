80be775ebcfab01b3180d4be44872d1218f96995
sunny1901kumar-sys <sunny1901kumar@gmail.com>
2026-08-28 14:01:03 +0000

    for each

---
+++ 05_iterations/six.js
@@ -0,0 +1,27 @@
+// const coding = ["js","ruby","java","py"]
+
+// const values = coding.forEach((item) => {
+//     // console.log(item);
+//     return item;
+// })
+
+// console.log(values);0
+
+
+const myNumber = [1,2,3,4,5,6,7,8,9,10];
+
+// let newNums = myNumber.filter( (num) => {
+//     return num>4
+// } )
+
+
+// const newNums = []
+// myNumber.forEach((num) => {
+//     if(num>4){
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+
