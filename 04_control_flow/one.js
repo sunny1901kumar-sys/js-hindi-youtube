bb3334a60318dc3b33e88426e9f1d60298a6e171
sunny1901kumar-sys <sunny1901kumar@gmail.com>
2026-08-28 08:32:58 +0000

    conditonal statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,51 @@
+// If statement
+
+const isUserLoggedIn = true
+const temperature  = 54
+
+
+// if(temperature === 41){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Executed");
+
+const score = 300
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+const balance  = 100
+
+// if(balance > 50) console.log("test"), console.log("test2");
+
+// if(balance < 500){
+//     console.log("less than 500");
+// }
+// else if(balance < 750){
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("less than 1200")
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2 ===3){
+    console.log("Allow to buy course");
+}
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
+
+
