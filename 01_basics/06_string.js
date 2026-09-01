c320784326e1e61daa6cd3d9073236a2daabb292
sunny1901kumar-sys <sunny1901kumar@gmail.com>
2026-09-01 13:14:05 +0000

    string and string methods

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hiteshhc')
+const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -13,7 +13,7 @@ const gameName = new String('hiteshhc')
 // console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+console.log(gameName.indexOf('t'));                                                                                                         
 
 const newString  = gameName.substring(0,4);
 console.log(newString);
@@ -25,7 +25,12 @@ const newStringOne = "   sunny   "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = " https:// hitesh.com/hiteshcahoudhat"
+const url = "https:// hitesh.com/hiteshc%20houdhary"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sunny'))
+
+console.log(gameName.split('-'));
 
 
 
