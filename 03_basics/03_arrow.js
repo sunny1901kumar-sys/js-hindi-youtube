5085c81b594472bc5d328477b8f54badbece12f8
sunny1901kumar-sys <sunny1901kumar@gmail.com>
2026-09-09 14:26:04 +0000

    this

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -36,7 +36,7 @@ const chai = () => {
 
 // }
 // const addTwo = (num1,num2) =>  num1 + num2
-// const addTwo = (num1,num2) => ( num1 + num2)
+// const addTwo = (num1,num2) => (num1 + num2)
 
 const addTwo = (num1,num2) =>({username: "sunny"})
    
